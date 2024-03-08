e8df0734c2d722458ea3dfd117ddf931653d1981
8e8b2c <8e8b2c@proton.me>
2024-03-08 17:05:06 +0000

    holo host connect

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -14,9 +14,12 @@
 <script lang="ts">
 import { defineComponent, computed } from 'vue';
 import { AppAgentClient, AppAgentWebsocket } from '@holochain/client';
+import WebSdkApi, { ChaperoneState } from '@holo-host/web-sdk';
 import '@material/mwc-circular-progress';
 import '@material/mwc-button';
 import Ping from './ping/ping/Ping.vue';
+
+const IS_HOLO_HOSTED = import.meta.env.VITE_IS_HOLO_HOSTED;
 
 export default defineComponent({
   components: {
@@ -34,7 +37,28 @@ export default defineComponent({
   },
   async mounted() {
     // We pass an unused string as the url because it will dynamically be replaced in launcher environments
-    this.client = await AppAgentWebsocket.connect(new URL('https://UNUSED'), 'ping');
+    if (IS_HOLO_HOSTED) {
+
+      const holoClient = await WebSdkApi.connect({
+        chaperoneUrl: 'http://localhost:24274',
+      });
+      await new Promise<void>(resolve => {
+        const handleHoloChaperoneStateChange = (state: ChaperoneState) => {
+          if (state.uiState.isVisible) return;
+          if (!state.agentState.isAvailable) return;
+
+          if (state.agentState.isAnonymous) holoClient.signUp({});
+          else resolve();
+        };
+        holoClient.on('chaperone-state', handleHoloChaperoneStateChange);
+        if (holoClient.chaperoneState) handleHoloChaperoneStateChange(holoClient.chaperoneState);
+      })
+
+      this.client = holoClient as unknown as AppAgentWebsocket;
+    } else {
+      this.client = await AppAgentWebsocket.connect(new URL('https://UNUSED'), 'ping');
+    }
+
     this.loading = false;
   },
   provide() {
